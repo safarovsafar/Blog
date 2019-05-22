9dee9f42a37d1ad2be2af1a71398e68ed5b256b1
safarovsafar <safarov7safar@gmail.com>
2019-05-22 23:24:41 +0500

    shot

--- app.js
+++ app.js
@@ -5,12 +5,29 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vest" +
+    "ibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum " +
+    "lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pre" +
+    "tium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semp" +
+    "er risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae t" +
+    "ortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae" +
+    ". Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vest" +
+    "ibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum " +
+    "lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pre" +
+    "tium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semp" +
+    "er risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae t" +
+    "ortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae" +
+    ". Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra " +
+    "justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing" +
+    " elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sap" +
+    "ien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum variu" +
+    "s sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum do" +
+    "lor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque." +
+    " Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fri" +
+    "ngilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam lib" +
+    "ero.";
 
 const app = express();
 
@@ -18,9 +35,7 @@ const posts = [];
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -31,15 +46,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {
-    aboutContent: aboutContent
-  });
+  res.render("about", {aboutContent: aboutContent});
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {
-    contactContent: contactContent
-  });
+  res.render("contact", {contactContent: contactContent});
 });
 app.get("/compose", (req, res) => {
   res.render("compose");
@@ -53,7 +64,7 @@ app.post('/compose', (req, res) => {
   res.redirect("/");
 });
 
-app.get('/posts/:postName', (req,res) => {
+app.get('/posts/:postName', (req, res) => {
   const requestedTitle = _.lowerCase(req.params.postName);
   posts.forEach((post) => {
     const storedTitle = post.title;
@@ -65,5 +76,5 @@ app.get('/posts/:postName', (req,res) => {
 });
 
 app.listen(3002, function () {
-  console.log("Server started on port 3000");
+  console.log("3000");
 });
